7524fac6c08b1f97ae2cb200ff7151ace3b54193
you-see-seeyou <434886232@qq.com>
2022-07-12 17:25:45 +0800

    数组指针测试

---
+++ test.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int change_a(int a[],int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        a[i]+=1;
+    }
+}
+
+int change_p(int *a,int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        a[i]+=1;
+    }
+}
+int main(int argc, char const *argv[])
+{
+    int a[5] = {1,2,3,4,5};
+    const int *p = a;
+    p++;
+    int * const q = a;
+    (*q)++;
+    //change_a(a,5);
+    change_p(a,5);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("a[%d] = %d\n",i,a[i]);
+    }
+    return 0;
+}
